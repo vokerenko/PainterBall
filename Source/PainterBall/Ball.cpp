c372d3e3c0b64b3fb0a952a456932eaccda508c2
Oleh Riabov <vokerenko@gmail.com>
2023-08-26 02:10:06 +0300

    Added ABall class and blueprint

---
+++ Source/PainterBall/Ball.cpp
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Ball.h"
+
+// Sets default values
+ABall::ABall()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	
+	StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Sphere"));
+	SetRootComponent(StaticMesh);
+	StaticMesh->SetSimulatePhysics(true);
+}
+
+// Called when the game starts or when spawned
+void ABall::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
+// Called every frame
+void ABall::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ABall::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
